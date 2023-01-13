cb3444673fc0092ad67ccbd1c4406c99c31597b0
emmanuelolofintuyi <EmmanuelOlofintuyi@gmail.com>
2023-01-13 10:15:40 -0500

    added a method to merge sort that lets me merge sort list instead of arrays

--- javaPractice/mergeSort_quickSort/megeSort/MergeSort.java
+++ javaPractice/mergeSort_quickSort/megeSort/MergeSort.java
@@ -1,5 +1,7 @@
 package mergeSort_quickSort.megeSort;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MergeSort {
@@ -10,13 +12,20 @@ public class MergeSort {
             numbers[i] = rand.nextInt(1000000);
          }
 
-        System.out.println("Before");
-         printArray(numbers);
+         List<Integer> numbersList= new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            numbersList.add( rand.nextInt(1000000));
+        }
 
-         mergeSort(numbers);
+        System.out.println("Before");
+         //printArray(numbers);
+        printList(numbersList);
+
+         mergeSortList(numbersList);
 
         System.out.println("\nAfter:");
-        printArray(numbers);
+        //printArray(numbers);
+        printList(numbersList);
 
     }
 
@@ -74,9 +83,70 @@ public class MergeSort {
 
     }
 
+    public static void mergeSortList(List<Integer> inputList){
+        int inputLength = inputList.size();
+
+        if (inputLength<2){
+            return;
+        }
+        int midIndex = inputLength/2;
+        List<Integer> leftHalf = new ArrayList<>();
+        List<Integer> rightHalf = new ArrayList<>();
+
+        for(int i = 0; i < midIndex; i++){
+            leftHalf.add(i, inputList.get(i));
+        }
+        for(int i = midIndex; i < inputLength; i++){
+            rightHalf.add(i-midIndex, inputList.get(i));
+        }
+
+        mergeSortList(leftHalf);
+        mergeSortList(rightHalf);
+        mergeList(inputList, leftHalf, rightHalf);
+    }
+
+    private static void mergeList(List<Integer> inputList, List<Integer> leftHalf, List<Integer> rightHalf){
+        int leftSize = leftHalf.size();
+        int rightSize = rightHalf.size();
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while(i<leftSize && j < rightSize){
+            if (leftHalf.get(i)<=rightHalf.get(j)){
+                inputList.set(k, leftHalf.get(i));
+                i++;
+            }
+            else{
+                inputList.set(k, rightHalf.get(j));
+                j++;
+            }
+            k++;
+        }
+        while (i < leftSize){
+            inputList.set(k, leftHalf.get(i));
+            i++;
+            k++;
+        }
+
+        while (j < rightSize){
+            inputList.set(k,rightHalf.get(j));
+            j++;
+            k++;
+        }
+
+    }
+
+
     public static void printArray(int[] numbers){
         for(int i = 0; i < numbers.length; i++){
             System.out.println(numbers[i]);
+        }
+    }
+
+    public static void printList(List<Integer> numbers){
+        for(int i = 0; i < numbers.size(); i++){
+            System.out.println(numbers.get(i));
         }
     }
 }
